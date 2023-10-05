24db095450c8913a125a1b9367b6002d51201587
Miro Šimić <34944272+MiroTheMighty@users.noreply.github.com>
2023-10-05 20:20:54 +0200

    Vježba 1
    Zadatak 1

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, s;
+	std::cout << "Unesi dva broja" << std::endl;
+	std::cin >> a >> b;
+	s = a + b;
+	std::cout << "Suma je: " << s << std::endl;
+}
