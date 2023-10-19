4577f6ca71bbc3902692041dd9b829c85e99798c
Miro Šimić <34944272+MiroTheMighty@users.noreply.github.com>
2023-10-19 20:45:13 +0200

    Add files via upload

---
+++ Vježba 3/Zadatak 1 H.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<vector>
+
+void UnosVektora(std::vector<int>& v, int n);
+void UnosVektoraPar(std::vector<int>& v, int a, int b);
+void IspisVektora(std::vector<int> v);
---
+++ Vježba 3/Zadatak 1 Implementacija.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include"Zadatak 1 H.h"
+
+void UnosVektora(std::vector<int>& v, int n) {
+	for (int i = 0; i < n; i++) {
+		std::cout << "Unesite " << i + 1 << " element vektora: ";
+		std::cin >> v[i];
+	}
+
+	std::cout << std::endl;
+}
+
+void UnosVektoraPar(std::vector<int>& v, int a, int b) {
+	int n;
+	std::cout << "Unesite element vektora u intervalu " << a << "-" << b << ": ";
+	std::cin >> n;
+
+	while (n >= a && n <= b) {
+		v.push_back(n);
+		std::cout << "Unesite element vektora u intervalu " << a << "-" << b << ": ";
+		std::cin >> n;
+	}
+
+	std::cout << std::endl;
+}
+
+void IspisVektora(std::vector<int> v) {
+	for (int i = 0; i < v.size(); i++) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+}
