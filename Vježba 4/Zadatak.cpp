9639eed60066170ab0c3f4439da5b5dba2eec5d0
Miro Šimić <34944272+MiroTheMighty@users.noreply.github.com>
2023-11-01 22:36:46 +0100

    Update Zadatak.cpp

--- Vježba 4/Zadatak.cpp
+++ Vježba 4/Zadatak.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<algorithm>
 
 int BrojSubStringova(std::string string, std::string subString) {
 	int br = 0;
-	
+
 	for (size_t a = string.find(subString); a != std::string::npos; a = string.find(subString, a + subString.size()))
 	{
 		br++;
@@ -18,9 +20,28 @@ void MakniRazmake(std::string& string) {
 			string[a] = ',';
 			string[a + 1] = ' ';
 		}
-		else if (string[a] == ' ' && string[a+1] == ' ') {
-			string.erase(a+1, 1);
+		else if (string[a] == ' ' && string[a + 1] == ' ') {
+			string.erase(a + 1, 1);
 		}
+	}
+}
+
+void PreokreniStringove() {
+	int n;
+	std::cout << "Unesite broj stringova: ";
+	std::cin >> n;
+	std::vector<std::string> str(n);
+
+	for (int i = 0; i < n; i++) {
+		std::cout << "Unesite " << i + 1 << " string: ";
+		std::cin >> str[i];
+		std::reverse(str[i].begin(), str[i].end());
+	}
+
+	std::sort(str.begin(), str.end());
+
+	for (int i = 0; i < n; i++) {
+		std::cout << str[i] << std::endl;
 	}
 }
 
@@ -34,6 +55,7 @@ int main() {
 	std::cout << n << std::endl;
 	MakniRazmake(string1);
 	std::cout << string1 << std::endl;
+	PreokreniStringove();
 
 	return 0;
 }
